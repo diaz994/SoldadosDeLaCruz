831da8e73fe60cedb15d58a5a85490955db7d4b5
Victor Diaz <vd31192@gmail.com>
2014-12-22 13:44:46 -0500

    Adding Parse
    Adding the Parse Library. Still deciding if it is going to be used or
    now or if I will build my own backend.

--- app/src/main/java/org/soldiersofthecross/soldadosdelacruz/MainActivity.java
+++ app/src/main/java/org/soldiersofthecross/soldadosdelacruz/MainActivity.java
@@ -45,6 +45,8 @@ public class MainActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+
     }
 
     @Override
